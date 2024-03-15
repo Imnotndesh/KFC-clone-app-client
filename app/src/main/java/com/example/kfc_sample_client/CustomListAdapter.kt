64a9045c5b2429394343b2d41af673eb3f5e8548
Imnotndesh <slateinbox@gmail.com>
2024-03-15 22:31:51 +0300

    first baseline

--- app/src/main/java/com/example/kfc_sample_client/CustomListAdapter.kt
+++ app/src/main/java/com/example/kfc_sample_client/CustomListAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.kfc_sample_client
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
@@ -8,17 +9,32 @@ import android.widget.BaseAdapter
 import android.widget.ListView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
-
 class CustomListAdapter(private val context: Context ) : BaseAdapter(){
+    override fun getCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItem(position: Int): Any {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemId(position: Int): Long {
+        TODO("Not yet implemented")
+    }
+
+    @SuppressLint("InflateParams")
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        val viewHolder : ViewHolder
+        val viewHolder : CustomListAdapter.ViewHolder
         if (convertView == null){
             val inflater = LayoutInflater.from(context)
-            val view = inflater.inflate(R.layout.list_item, null)
+            val view = inflater.inflate(
+                R.layout.list_item,
+                null
+            )
             viewHolder = ViewHolder(view)
             view.tag = viewHolder
         }else{
-            pass
+            println("sumn")
         }
         return convertView!!
     }
--- app/src/main/java/com/example/kfc_sample_client/profile_page.kt
+++ app/src/main/java/com/example/kfc_sample_client/profile_page.kt
@@ -5,13 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ListView
+
 class profile_page : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile_page, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val profile_stats = view.findViewById<ListView>(R.id.profile_stats_display)
+        val adapter = CustomListAdapter(requireContext())
+        profile_stats.adapter = adapter
+
+    }
+
 }
