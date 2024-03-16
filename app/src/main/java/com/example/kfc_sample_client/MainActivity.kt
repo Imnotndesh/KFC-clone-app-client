fe6ec6bd57cc661a19467556920639d9c4a99a19
Imnotndesh <slateinbox@gmail.com>
2024-03-16 14:04:01 +0300

    trying to fix view switcher

--- app/src/main/java/com/example/kfc_sample_client/MainActivity.kt
+++ app/src/main/java/com/example/kfc_sample_client/MainActivity.kt
@@ -3,13 +3,30 @@ package com.example.kfc_sample_client
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navigationView: BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        navigationView = findViewById(R.id.main_navigation)
+        navigationView.setOnItemSelectedListener{ item -> val selectedFragment: Fragment? = when (item.itemId) {
+            R.id.navigation_home -> homePage()
+            R.id.navigation_order -> orders_page()
+            R.id.navigation_profile -> profile_page()
+            else -> null
+        }
+        if (selectedFragment != null){
+            val fragmentManager = supportFragmentManager
+            val transaction = fragmentManager.beginTransaction()
+            transaction.replace(R.id.main_fragment_container,selectedFragment)
+            transaction.commit()
+        }
+            true
+        }
     }
+
 }
+
